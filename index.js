59508bcf7a1eff55a33dffc6523fe018f8d3332d
Ivan Benitez <ivanmatias300@gmail.com>
2022-09-21 00:12:52 -0300

    Servidor creado

--- index.js
+++ index.js
@@ -1,4 +1,21 @@
+const express = require('express');
 
 
+// Creacion del servidor de Express
 
-console.log("Back");
+const app = express();
+
+
+// Rutas
+app.get('/', (req, res) => {
+    res.json({
+        ok: true
+    })
+});
+
+
+// Puerto donde se escuchan las peticiones
+
+app.listen( 4000, () => {
+    console.log(`Servidor corriendo en puerto ${ 4000 }`);
+});
