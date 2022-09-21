f6e01da337371ab4e39960233f3f185f90548846
Ivan Benitez <ivanmatias300@gmail.com>
2022-09-21 13:06:27 -0300

    express validator agregado

--- routes/auth.js
+++ routes/auth.js
@@ -5,6 +5,7 @@
 
 
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
 
 const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/auth');
@@ -15,7 +16,14 @@ const { crearUsuario, loginUsuario, revalidarToken } = require('../controllers/a
 
 
 
-router.post('/new', crearUsuario );
+router.post(
+    '/new',
+    [
+        check('name', 'El nombre es obligatorio').not().isEmpty(),
+        check('email', 'El email es obligatorio').isEmail(),
+    ],
+    crearUsuario 
+    );
 
 
 
