a7b52e2613ac77fa474db438b70c46995c79f40e
Ivan Benitez <ivanmatias300@gmail.com>
2022-09-30 17:49:31 -0300

    Prueba Github y errores en pantalla

--- passport.js
+++ passport.js
@@ -6,7 +6,7 @@ const passport = require('passport');
 passport.use(new GitHubStrategy({
     clientID: process.env.GITHUB_CLIENT_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET,
-    callbackURL: "/api/auth/github/callback"
+    callbackURL: "https://teamhaskell.herokuapp.com/api/auth/github/callback"
   },
   function(accessToken, refreshToken, profile, done) {
     // Usuario.findOrCreate({ githubId: profile.id }, function (err, user) {
